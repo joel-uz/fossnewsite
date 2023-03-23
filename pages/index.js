11659a0be1a629a36b9c548ab8ab114b00fa5bc6
joel john <j2joelgeorgejohn@gmail.com>
2023-03-23 13:51:51 +0530

    bug fixes

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return(
     <div>
         <h1>FOSS WEBSITE</h1>
-        <a href={'desktop5/index'}>Click here to go the desktop</a>
+        <a href={'desktop5/'}>Click here to go the desktop</a>
     </div>
   )
 }
